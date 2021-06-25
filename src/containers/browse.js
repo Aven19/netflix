aca452095d3d8808e95e566293ec04b500af4248
dwarf <ixltech.aven@gmail.com>
2021-06-26 01:42:45 +0530

    Browse page main header completed heading towards card

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,10 +2,11 @@ import React, { useState, useContext, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
 import { Header, Loading } from "../components";
-import * as ROUTES from '../constants/routes';
-import logo from '../logo.svg';
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
 
 export function BrowseContainer({ slides }) {
+  const [searchTerm, setSearchTerm] = useState("");
   const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
 
@@ -21,21 +22,44 @@ export function BrowseContainer({ slides }) {
   return profile.displayName ? (
     <>
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
-      <Header src="joker1">
-      <Header bg={false}>
-        <Header.Frame>
-          <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-        </Header.Frame>
-      </Header>
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header bg={false}>
+          <Header.Frame>
+            <Header.Group>
+              <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+              <Header.TextLink>Series</Header.TextLink>
+              <Header.TextLink>Films</Header.TextLink>
+            </Header.Group>
+            <Header.Group>
+              <Header.Search
+                searchTerm={searchTerm}
+                setSearchTerm={searchTerm}
+              />
+              <Header.Profile>
+                <Header.Picture src={user.photoURL} />
+                <Header.Dropdown>
+                  <Header.Group>
+                    <Header.Picture src={user.photoURL} />
+                    <Header.TextLink>{user.displayName}</Header.TextLink>
+                  </Header.Group>
+                  <Header.Group>
+                    <Header.TextLink onClick={() => firebase.auth().signOut()}>
+                      Sign out
+                    </Header.TextLink>
+                  </Header.Group>
+                </Header.Dropdown>
+              </Header.Profile>
+            </Header.Group>
+          </Header.Frame>
+        </Header>
         <Header.Feature>
-          <Header.FeatureCallOut>
-            Watch Joker Now
-          </Header.FeatureCallOut>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
           <Header.Text>
             {
               "Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a futile attempt to feel like he's part of the world around him."
             }
           </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
     </>
